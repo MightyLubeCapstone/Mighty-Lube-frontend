4978f5f8997050f4b499010b8033744477ee7e21
Jacob Scott <jscott43@tntech.edu>
2024-10-29 15:56:26 -0500

    formatting

--- mighty_lube/lib/main.dart
+++ mighty_lube/lib/main.dart
@@ -14,18 +14,19 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Column(
-          children: [
-            Container(
-            width: double.infinity,
-            color: const Color.fromARGB(255, 87, 154, 246),
-            /*funny color color: const Color.fromARGB(255, 66,6,159),*/
-              child:
-                SvgPicture.asset('assets/WhiteML_Logo-w-tag-vector.svg',
-                width: 220,
-                height: 220,
-                color: Color.fromARGB(255, 249, 249, 250),),
-            ),
-          ],
+            children: [
+              Container(
+                width: double.infinity,
+                color: const Color.fromARGB(255, 87, 154, 246),
+                /*funny color color: const Color.fromARGB(255, 66,6,159),*/
+                child: SvgPicture.asset(
+                  'assets/WhiteML_Logo-w-tag-vector.svg',
+                  width: 220,
+                  height: 220,
+                  color: Color.fromARGB(255, 249, 249, 250),
+                ),
+              ),
+            ],
           ),
         ),
       ),
