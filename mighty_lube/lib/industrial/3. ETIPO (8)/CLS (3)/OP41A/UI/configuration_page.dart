57630a912578085f36bf6de5d80048a6572e7c86
addison goforth <addison@QWRkaXNvbnMtTWFjQm9vay1Qcm8.local>
2024-12-08 00:28:28 -0600

    Incase I break something pt2

---
+++ mighty_lube/lib/industrial/3. ETIPO (8)/CLS (3)/OP41A/UI/configuration_page.dart
@@ -0,0 +1,346 @@
+import 'package:flutter/material.dart';
+import 'package:mighty_lube/application/UI/applicationHome.dart';
+import 'package:mighty_lube/protien/protienHome.dart';
+
+class ConfigurationSection extends StatefulWidget {
+  const ConfigurationSection({super.key});
+
+  @override
+  State<ConfigurationSection> createState() => _ConfigurationSectionState();
+}
+
+class _ConfigurationSectionState extends State<ConfigurationSection> {
+  int itemCount = 1; // Default count
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        buildBreadcrumbNavigation(context),
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.all(20.0),
+            children: [
+              buildGradientButton(context, 'General Information',
+                  buildGeneralInformationContent()),
+              buildGradientButton(context, 'Customer Power Utilities',
+                  buildCustomerPowerUtilitiesContent()),
+            ],
+          ),
+        ),
+        const SizedBox(height: 20),
+        buildCounterSection(),
+        const SizedBox(height: 20),
+        buildGradientConfiguratorButton(),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+
+  Widget buildGradientButton(
+      BuildContext context, String title, Widget content) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        gradient: const LinearGradient(
+          colors: [Color(0xFF73A1F9), Color(0xFF4D86F5)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: TextButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            builder: (context) => FractionallySizedBox(
+              heightFactor: 0.90,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 20),
+                      Center(
+                        child: Container(
+                          width: 50,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      content,
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Save',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildGeneralInformationContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildSectionTitle('Conveyor Details'),
+        buildDropdownField('Conveyor Chain Size', [
+          'X348 Chain (3”)',
+          'X458 Chain (4”)',
+          'OX678 Chain (6”)',
+          'Other'
+        ]),
+        buildDropdownField('Protein: Chain Manufacturer', [
+          'Green Line',
+          'Frost',
+          'M&M',
+          'Stork',
+          'Meyn',
+          'Linco',
+          'DC',
+          'Merel',
+          'D&F',
+          'Other'
+        ]),
+        buildSectionDivider(),
+        buildSectionTitle('Environmental Details'),
+        buildDropdownField('Is the Conveyor "__" at Planned Install Location',
+            ['Loaded', 'Unloaded']),
+        buildDropdownField('Is this a Drip Line', ['Yes', 'No']),
+      ],
+    );
+  }
+
+  Widget buildCustomerPowerUtilitiesContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildDropdownField('Operating Voltage - 3 Phase: (Volts/hz)',
+            ['Option 1', 'Option 2', 'Option 3']),
+        buildDropdownField(
+          'Confirm Installation Clearance of: Minimum of 2\' (.61m) for clearance of Motor Height from Rail AND Motor Gear Housing assembly width',
+          ['Yes', 'No'],
+        ),
+        buildDropdownField('3-Station Push Button Switch', ['Yes', 'No']),
+      ],
+    );
+  }
+
+  Widget buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.blueAccent,
+        ),
+      ),
+    );
+  }
+
+  Widget buildSectionDivider() {
+    return const Divider(
+      color: Colors.grey,
+      thickness: 1,
+      height: 30,
+    );
+  }
+
+  Widget buildDropdownField(String label, List<String> options) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: DropdownButtonFormField<String>(
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        items: options
+            .map((option) => DropdownMenuItem<String>(
+                  value: option,
+                  child: Text(option),
+                ))
+            .toList(),
+        onChanged: (value) {},
+      ),
+    );
+  }
+
+  Widget buildCounterSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: () {
+            setState(() {
+              if (itemCount > 1) itemCount--;
+            });
+          },
+          icon: const Icon(Icons.remove_circle_outline,
+              color: Colors.blue, size: 30),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            '$itemCount',
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            setState(() {
+              itemCount++;
+            });
+          },
+          icon: const Icon(Icons.add_circle_outline,
+              color: Colors.blue, size: 30),
+        ),
+      ],
+    );
+  }
+
+  Widget buildGradientConfiguratorButton() {
+    return Container(
+      height: 50,
+      width: MediaQuery.of(context).size.width * 0.7, // Shorter width
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        gradient: const LinearGradient(
+          colors: [Color(0xFF73A1F9), Color(0xFF4D86F5)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: TextButton(
+        onPressed: () {
+          // Add your "Add to Configurator" logic here
+        },
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: const Text(
+          'ADD TO CONFIGURATOR',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildBreadcrumbNavigation(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.home, color: Colors.blue),
+          onPressed: () {
+            // Navigate to the home page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ApplicationPage()),
+            );
+          },
+        ),
+        const Text(' > '),
+        GestureDetector(
+          onTap: () {
+            // Navigate to the Protein page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ProteinHome()),
+            ); // Replace with navigation to your Protein page
+          },
+          child: const Text(
+            'Protein',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
