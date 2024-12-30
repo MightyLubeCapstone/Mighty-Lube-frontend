197420fff424d31f4ff3beed4acf4c5b1d27ba41
Baymax <addisonbg16@gmail.com>
2024-12-30 15:29:51 -0500

    Fixing branch in VSCode

--- mighty_lube/lib/industrial/industrialHome.dart
+++ mighty_lube/lib/industrial/industrialHome.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mighty_lube/app_bar.dart';
+import 'package:mighty_lube/application/UI/applicationHome.dart';
 import 'package:mighty_lube/dashboard/UI/dashboard.dart';
-import 'package:mighty_lube/dashboard/UI/profile.dart';
+import 'package:mighty_lube/drawer.dart';
 
 class HeaderLogo extends StatelessWidget {
   const HeaderLogo({super.key});
@@ -10,12 +12,12 @@ class HeaderLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 180, // Larger logo area
+      height: 180,
       color: const Color.fromARGB(255, 87, 154, 246),
       child: Center(
         child: SvgPicture.asset(
           'assets/WhiteML_Logo-w-tag-vector.svg',
-          width: 100, // Adjust width for larger logo
+          width: 100,
           height: 150,
           color: const Color.fromARGB(255, 249, 249, 250),
         ),
@@ -27,126 +29,213 @@ class HeaderLogo extends StatelessWidget {
 class IndustrialHome extends StatelessWidget {
   const IndustrialHome({super.key});
 
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize:
-            const Size.fromHeight(180), // Adjusted for larger header logo
-        child: Column(
-          children: [
-            // Header with logo and icons
-            Stack(
-              children: [
-                const HeaderLogo(),
-                Positioned(
-                  top: 60, // Adjust position for icons
-                  right: 10,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.account_circle,
-                            color: Colors.white),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ProfilePage()),
-                          );
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.shopping_cart,
-                            color: Colors.white),
-                        onPressed: () {
-                          // Add Cart functionality
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Industrial',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(height: 0),
-                  // Breadcrumb navigation with home icon
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const DashboardPage()),
-                          );
-                        },
-                        child: const Icon(
-                          Icons.home,
-                          color: Colors.blue,
-                        ),
-                      ),
-                      const Text(' > ',
-                          style: TextStyle(color: Colors.black54)),
-                      const Text(
-                        'Application',
-                        style: TextStyle(color: Colors.black54),
-                      ),
-                      const Text(' > ',
-                          style: TextStyle(color: Colors.black54)),
-                      const Text(
-                        'Industrial',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      appBar: const CustomAppBar(
+        link: ApplicationPage(),
+        customIcon: Icons.description,
       ),
+      drawer: const CustomDrawer(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Stacked vertical list of images with titles
+            // Breadcrumb Navigation
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    // Navigate to Home
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const DashboardPage()),
+                    );
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.home,
+                        color: Colors.blue, // Blue color for the icon
+                      ),
+                      SizedBox(width: 4), // Space between icon and arrow
+                    ],
+                  ),
+                ),
+                const Text(' > '),
+                GestureDetector(
+                  onTap: () {
+                    // Navigate to the Application page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ApplicationPage()),
+                    );
+                  },
+                  child: const Text(
+                    'Industrial',
+                    style: TextStyle(
+                      color: Colors.blue, // Blue color for clickable text
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+
+            // Vertical list of images with text
             Expanded(
               child: ListView(
                 children: [
-                  _buildClickableImageCard(
+                  _buildClickableImageCard( //CC5 Chain (2)
                     context: context,
-                    title: 'Food Grade Cleaner OP-8SS',
-                    imagePath: 'assets/',
+                    title: 'CC5 Chain (2)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/CC5/CC5.png',
                     onTap: () {
                       // Navigate to Industrial page
-                      // Replace the placeholder with your navigation code
-                      // Navigator.push(context,MaterialPageRoute(builder: (context) => const FGCOPage()),);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()), 
+                      );
                     },
                   ),
-                  _buildClickableImageCard(
+                  _buildClickableImageCard( //Chain On Edge Drag Line (3)
                     context: context,
-                    title: 'Food Grade Lubrication and Monitor',
-                    imagePath: 'assets/',
+                    title: 'Chain On Edge Drag Line (3)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Title.png',
                     onTap: () {
-                      //Navigator.push(context,MaterialPageRoute(builder: (context) => const FGLMPage()),);
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //Enclosed Track Inverted Power Only and PF (8)
+                    context: context,
+                    title: 'Enclosed Track Inverted Power Only and PF (8)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //Enclosed Track Overhead Power Only and P&F (10)
+                    context: context,
+                    title: 'Enclosed Track Overhead Power Only and P&F (10)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //Flat Top (4)
+                    context: context,
+                    title: 'Flat Top (4)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //Free Carrier (2)
+                    context: context,
+                    title: 'Free Carrier (2)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Free Carrier (2)/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //Free Rail Overhead Or Inverted (6)
+                    context: context,
+                    title: 'Free Rail Overhead Or Inverted (6)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //In Floor Tow Line (2)'
+                    context: context,
+                    title: 'In Floor Tow Line (2)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Free Carrier (2)/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //In-Board Roller Chain (3)
+                    context: context,
+                    title: 'In-Board Roller Chain (3)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Free Carrier (2)/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //Over Head Power Rail L-Beam (20)
+                    context: context,
+                    title: 'Over Head Power Rail L-Beam (20)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
+                    },
+                  ),
+                  _buildClickableImageCard( //Power and Free Overhead or Inverted (17)
+                    context: context,
+                    title: 'Power and Free Overhead or Inverted (17)',
+                    imagePath: '/Users/addison/Mighty-Lube-frontend/mighty_lube/assets/industrial/Title.png',
+                    onTap: () {
+                      //Will need to be changed later
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const IndustrialHome()),
+                      );
                     },
                   ),
                 ],
               ),
+              
             ),
           ],
         ),
@@ -170,7 +259,7 @@ class IndustrialHome extends StatelessWidget {
             Container(
               height: 250,
               width: double.infinity,
-              color: Colors.black12, // Fallback background color
+              color: Colors.black12,
               child: Image.asset(
                 imagePath,
                 fit: BoxFit.cover,
