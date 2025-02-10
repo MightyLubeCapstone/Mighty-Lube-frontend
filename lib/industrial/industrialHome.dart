2f2b56a4926f934803529a8a860ae6f963d88803
Wes Talley <wesatalley@gmail.com>
2025-02-10 16:31:57 -0600

    Search functionality works! :D

--- lib/industrial/industrialHome.dart
+++ lib/industrial/industrialHome.dart
@@ -4,6 +4,7 @@ import 'package:mighty_lube/app_bar.dart';
 import 'package:mighty_lube/application/UI/applicationHome.dart';
 import 'package:mighty_lube/dashboard/UI/dashboard.dart';
 import 'package:mighty_lube/drawer.dart';
+import 'product_list.dart';
 
 import 'package:mighty_lube/industrial/1.%20CC5C%20(2)/CLS%20(2)/CLS.dart';
 import 'package:mighty_lube/industrial/10.%20OHPRLB%20(20)/subfolders.dart';
@@ -46,7 +47,98 @@ class IndustrialHome extends StatefulWidget {
 }
 
 class _IndustrialHomeState extends State<IndustrialHome> {
+  // Helper function to filter cards based on the search query
   String searchQuery = '';
+
+  bool _filterCard(String title) {
+    return title.toLowerCase().contains(searchQuery.toLowerCase());
+  }
+
+  Widget _buildClickableImageCard({
+    required BuildContext context,
+    required String title,
+    required String imagePath,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(
+                    255, 87, 154, 246), // Same blue as breadcrumb nav
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12.0),
+                  topRight: Radius.circular(12.0),
+                ),
+              ),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            // Image
+            Container(
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: const Color.fromARGB(28, 50, 51, 52),
+                    width: 3.0), // More noticeable border
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(12.0),
+                  bottomRight: Radius.circular(12.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color.fromARGB(25, 54, 53, 53)
+                        .withOpacity(0.3), // More prominent shadow
+                    blurRadius: 10.0,
+                    offset: const Offset(0, 6),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(12.0),
+                  bottomRight: Radius.circular(12.0),
+                ),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Center(
+                      child: Text(
+                        'Image not found',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 14,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -135,97 +227,6 @@ class _IndustrialHomeState extends State<IndustrialHome> {
                     (searchQuery == '')
                         ? _buildDefaultCards()
                         : _buildDynamicList(),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  // Helper function to filter cards based on the search query
-  bool _filterCard(String title) {
-    return title.toLowerCase().contains(searchQuery.toLowerCase());
-  }
-
-  Widget _buildClickableImageCard({
-    required BuildContext context,
-    required String title,
-    required String imagePath,
-    required VoidCallback onTap,
-  }) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Title
-            Container(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(
-                    255, 87, 154, 246), // Same blue as breadcrumb nav
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(12.0),
-                  topRight: Radius.circular(12.0),
-                ),
-              ),
-              child: Text(
-                title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            // Image
-            Container(
-              height: 250,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(
-                    color: const Color.fromARGB(28, 50, 51, 52),
-                    width: 3.0), // More noticeable border
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(12.0),
-                  bottomRight: Radius.circular(12.0),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromARGB(25, 54, 53, 53)
-                        .withOpacity(0.3), // More prominent shadow
-                    blurRadius: 10.0,
-                    offset: const Offset(0, 6),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(12.0),
-                  bottomRight: Radius.circular(12.0),
-                ),
-                child: Image.asset(
-                  imagePath,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return const Center(
-                      child: Text(
-                        'Image not found',
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: 14,
-                          fontStyle: FontStyle.italic,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    );
-                  },
-                ),
               ),
             ),
           ],
@@ -361,6 +362,19 @@ class _IndustrialHomeState extends State<IndustrialHome> {
   }
 
   List<Widget> _buildDynamicList() {
-    return [];
+    return productList.entries
+        .where((entry) => _filterCard(entry.key))
+        .map((entry) => _buildClickableImageCard(
+              context: context,
+              title: entry.value.title,
+              imagePath: entry.value.imagePath,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => entry.value.callback),
+                );
+              },
+            ))
+        .toList();
   }
 }
--- lib/industrial/product_list.dart
+++ lib/industrial/product_list.dart
@@ -1,14 +1,15 @@
+import 'package:mighty_lube/industrial/1.%20CC5C%20(2)/CLS%20(2)/MLCC5CL/UI/MLCC5CL.dart';
+import 'package:mighty_lube/industrial/1.%20CC5C%20(2)/CLS%20(2)/OP40E/UI/OP40E.dart';
+import 'package:mighty_lube/industrial/2.%20COEDL%20(3)/CLS%20(3)/CDL/UI/CDL.dart';
+import 'package:mighty_lube/industrial/2.%20COEDL%20(3)/CLS%20(3)/MLCEL/UI/MLCEL.dart';
+import 'package:mighty_lube/industrial/3.%20ETIPO%20(8)/CCS%20(2)/O8074/UI/O8074.dart';
+import 'package:mighty_lube/industrial/3.%20ETIPO%20(8)/CCS%20(2)/OUN91/UI/OUN91.dart';
+import 'package:mighty_lube/industrial/3.%20ETIPO%20(8)/CLS%20(3)/9000INVL/UI/9000INVL.dart';
+import 'package:mighty_lube/industrial/3.%20ETIPO%20(8)/CLS%20(3)/OP41A/UI/OP41A.dart';
+import 'package:mighty_lube/industrial/3.%20ETIPO%20(8)/CLS%20(3)/OP48E/UI/OP48E.dart';
+import 'package:mighty_lube/industrial/3.%20ETIPO%20(8)/CMS%20(1)/MLAIO/UI/MLAIO.dart';
+
 import 'package:flutter/material.dart';
-import '1. CC5C (2)/CLS (2)/MLCC5CL/UI/MLCC5CL.dart';
-import '1. CC5C (2)/CLS (2)/OP40E/UI/OP40E.dart';
-import '2. COEDL (3)/CLS (3)/CDL/UI/CDL.dart';
-import '2. COEDL (3)/CLS (3)/MLCEL/UI/MLCEL.dart';
-import '3. ETIPO (8)/CCS (2)/OUN91/UI/OUN91.dart';
-import '3. ETIPO (8)/CCS (2)/O8074/UI/O8074.dart';
-import '3. ETIPO (8)/CLS (3)/9000INVL/UI/9000INVL.dart';
-import '3. ETIPO (8)/CLS (3)/OP41A/UI/OP41A.dart';
-import '3. ETIPO (8)/CLS (3)/OP48E/UI/OP48E.dart';
-import '3. ETIPO (8)/CMS (1)/MLAIO/UI/MLAIO.dart';
 
 class Product {
   final String title;
@@ -21,70 +22,72 @@ class Product {
   });
 }
 
-const Map<String, Product> productList = {
+final Map<String, Product> productList = {
   // 1. CC5
-  'Mighty Lube CC5 Chain Lubricator': Product(
+  'Mighty Lube CC5 Chain Lubricator': const Product(
     title: 'Mighty Lube CC5 Chain Lubricator',
     imagePath: 'assets/industrial/CC5 Chain (2)/CLS (2)/MLCCL.png',
     callback: MLCC5CLPage(),
   ),
-  'OP-40E': Product(
+  'OP-40E': const Product(
     title: 'OP-40E',
     imagePath: 'assets/industrial/CC5 Chain (2)/CLS (2)/OP-40E.png',
     callback: OP40EPage(),
   ),
   // 2. COEDL
-  'Caterillar Drive Lubricators': Product(
+  'Caterillar Drive Lubricators': const Product(
     title: 'Caterillar Drive Lubricators',
     imagePath:
         'assets/industrial/Chain On Edge Drag Line (3)/Conveyor Lub. Systems/CDL.png',
     callback: CDLPage(),
   ),
-  'Mighty Lube Chain on Edge Lubricator': Product(
+  'Mighty Lube Chain on Edge Lubricator': const Product(
     title: 'Mighty Lube Chain on Edge Lubricator',
     imagePath:
         'assets/industrial/Chain On Edge Drag Line (3)/Conveyor Lub. Systems/MLCEL.png',
     callback: MLCELPage(),
   ),
   // 3. ETIPO (8)
-  'Overhead Non-Powered Mighty Lube Rail Cleaners UN91, RW91': Product(
+  'Overhead Non-Powered Mighty Lube Rail Cleaners UN91, RW91': const Product(
     title: 'Overhead Non-Powered Mighty Lube Rail Cleaners UN91, RW91',
     imagePath:
         'assets/industrial/Enclosed Track Inverted Power Only and P&F (8)/Conveyor Cleaning Solutions (2)/UN91.png',
     callback: OUN91Page(),
   ),
-  'Overhead Non-Powered Mighty Lube Chain Cleaners 8074-B, 8075-': Product(
+  'Overhead Non-Powered Mighty Lube Chain Cleaners 8074-B, 8075-':
+      const Product(
     title: 'Overhead Non-Powered Mighty Lube Chain Cleaners 8074-B, 8075-',
     imagePath:
         'assets/industrial/Enclosed Track Inverted Power Only and P&F (8)/Conveyor Cleaning Solutions (2)/8074-B.png',
     callback: O8074Page(),
   ),
-  '9000INVL (Inverted) Series Enclosed Track Conveyor Lubricators': Product(
+  '9000INVL (Inverted) Series Enclosed Track Conveyor Lubricators':
+      const Product(
     title: '9000INVL (Inverted) Series Enclosed Track Conveyor Lubricators',
     imagePath:
         'assets/industrial/Enclosed Track Inverted Power Only and P&F (8)/Conveyor Lub. Systems (3)/9000INVL.png',
     callback: Page900INVL(),
   ),
-  'OP-41A Conveyor Lubricators': Product(
+  'OP-41A Conveyor Lubricators': const Product(
     title: 'OP-41A Conveyor Lubricators',
     imagePath:
         'assets/industrial/Enclosed Track Inverted Power Only and P&F (8)/Conveyor Lub. Systems (3)/OP-41A.png',
     callback: OP41APage(),
   ),
-  'OP-48E': Product(
+  'OP-48E': const Product(
     title: 'OP-48E',
     imagePath:
         'assets/industrial/Enclosed Track Inverted Power Only and P&F (8)/Conveyor Lub. Systems (3)/OP-48E.png',
     callback: OP48EPage(),
   ),
-  'Multi Line (Permanent) All IN ONE Monitoring + Lubrication': Product(
+  'Multi Line (Permanent) All IN ONE Monitoring + Lubrication': const Product(
     title: 'Multi Line (Permanent) All IN ONE Monitoring + Lubrication',
     imagePath:
         'assets/industrial/Enclosed Track Inverted Power Only and P&F (8)/Conveyor Monitor Systems (1)/Multi Line.png',
     callback: MLAIOPage(),
   ),
   // 4. ETOPO (10)
-  // 5. FT(40)
+  // 5. FT(4)
   // 6. FC(2)
   // 7. FROOI (6)
   // 8. IFTL (2)
