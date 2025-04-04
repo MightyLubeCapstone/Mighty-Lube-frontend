37ea3655c6d10df2addb405d7747505f0f28249c
Wes Talley <wesatalley@gmail.com>
2025-04-04 15:29:11 -0500

    Actual Android production build

--- android/app/src/main/kotlin/com/example/mighty_lube/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mighty_lube
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/mightylube/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mightylube
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
