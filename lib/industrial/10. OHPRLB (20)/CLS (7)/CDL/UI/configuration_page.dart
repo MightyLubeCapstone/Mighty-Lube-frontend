86b4532cf49ee1f8937ca08ea697534f8cff490f
Wes Talley <wesatalley@gmail.com>
2025-04-07 20:14:53 -0500

    10/CLS/CDL done

--- lib/industrial/10. OHPRLB (20)/CLS (7)/CDL/UI/configuration_page.dart
+++ lib/industrial/10. OHPRLB (20)/CLS (7)/CDL/UI/configuration_page.dart
@@ -3,7 +3,6 @@ import 'package:mighty_lube/application/UI/applicationHome.dart';
 import 'package:mighty_lube/industrial/10.%20OHPRLB%20(20)/CLS%20(7)/products.dart';
 import 'package:mighty_lube/helper_widgets.dart';
 
-
 class ConfigurationSection extends StatefulWidget {
   const ConfigurationSection({super.key});
 
@@ -17,66 +16,87 @@ class _ConfigurationSectionState extends State<ConfigurationSection> {
   final TextEditingController conveyorLength = TextEditingController();
   final TextEditingController conveyorSpeed = TextEditingController();
   final TextEditingController conveyorIndex = TextEditingController();
-  final TextEditingController operatingVoltage = TextEditingController();
+  final TextEditingController controlVoltage = TextEditingController();
+  final TextEditingController lubeEquipment = TextEditingController();
+  final TextEditingController lubeType = TextEditingController();
+  final TextEditingController lubeGrade = TextEditingController();
   final TextEditingController conductor4 = TextEditingController();
   final TextEditingController conductor7 = TextEditingController();
   final TextEditingController conductor2 = TextEditingController();
+  final TextEditingController conductor12 = TextEditingController();
+  final TextEditingController junctBoxQuant = TextEditingController();
+  final TextEditingController reservoirQuant = TextEditingController();
+  final TextEditingController specialOptions = TextEditingController();
+  final TextEditingController specs = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CommonWidgets.buildBreadcrumbNavigation(context,'>',const ApplicationPage(),'Products',const CLSProducts()),
+        CommonWidgets.buildBreadcrumbNavigation(context, '>',
+            const ApplicationPage(), 'Products', const CLSProducts()),
         Expanded(
           child: ListView(
             padding: const EdgeInsets.all(20.0),
             children: [
-              CommonWidgets.buildGradientButton(context, 'General Information',buildGeneralInformationContent()),
-              CommonWidgets.buildGradientButton(context, 'Customer Power Utilities',buildCustomerPowerUtilitiesContent()),
-              CommonWidgets.buildGradientButton(context, 'New/Existing Monitoring System',buildMonitoringFeatures()),
-              CommonWidgets.buildGradientButton(context, 'Conveyor Specifications',buildConveyorSpecifications()),
-              CommonWidgets.buildGradientButton(context, 'Controller',buildController()),
-              CommonWidgets.buildGradientButton(context, 'Wire',buildWire()),
+              CommonWidgets.buildGradientButton(context, 'General Information',
+                  buildGeneralInformationContent()),
+              CommonWidgets.buildGradientButton(
+                  context,
+                  'Customer Power Utilities',
+                  buildCustomerPowerUtilitiesContent()),
+              CommonWidgets.buildGradientButton(context,
+                  'Conveyor Specifications', buildConveyorSpecifications()),
+              CommonWidgets.buildGradientButton(
+                  context, 'Controller', buildController()),
+              CommonWidgets.buildGradientButton(context, 'Wire', buildWire()),
             ],
           ),
         ),
-       
         CommonWidgets.buildConfiguratorWithCounter(),
         const SizedBox(height: 20),
       ],
     );
   }
 
-//actual buttons w/ the questions :) 
+//actual buttons w/ the questions :)
 
   Widget buildGeneralInformationContent() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CommonWidgets.buildSectionTitle('Conveyor Details'),
+        CommonWidgets.buildSectionDivider(),
         CommonWidgets.buildDropdownField('Conveyor Chain Size', [
           'X348 Chain (3”)',
           'X458 Chain (4”)',
-          'OX678 Chain (6”)',
+          'X678 Chain (6”)',
+          '3/8" Log Chain',
           'Other'
         ]),
         CommonWidgets.buildDropdownField('Protein: Chain Manufacturer', [
-          'Green Line',
+          'Daifuku',
           'Frost',
-          'M&M',
-          'Stork',
-          'Meyn',
-          'Linco',
-          'DC',
-          'Merel',
-          'D&F',
+          'NKC',
+          'Pacline',
+          'Rapid',
+          'WEBB',
+          'Webb-Stiles',
+          'Wilkie Brothers',
           'Other'
         ]),
+        CommonWidgets.buildTextField("Conveyor Length", conveyorLength),
+        CommonWidgets.buildDropdownField("Conveyor Length Unit",
+            ['Feet', 'Inches', 'm Meter', 'mm Millimeter']),
+        CommonWidgets.buildDropdownField("Application Environment", [
+          "Ambient",
+          "Caustic (i.e. Phosphate/E-Coat, etc)",
+          "Oven",
+          "Washdown",
+          "Intrinsic",
+          "Food Grade",
+          "Other"
+        ]),
         CommonWidgets.buildSectionDivider(),
-        CommonWidgets.buildSectionTitle('Environmental Details'),
-        CommonWidgets.buildDropdownField('Is the Conveyor "__" at Planned Install Location',
-            ['Loaded', 'Unloaded']),
-        CommonWidgets.buildDropdownField('Is this a Drip Line', ['Yes', 'No']),
       ],
     );
   }
@@ -85,65 +105,8 @@ class _ConfigurationSectionState extends State<ConfigurationSection> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CommonWidgets.buildDropdownField('Operating Voltage - 3 Phase: (Volts/hz)',
-            ['Option 1', 'Option 2', 'Option 3']),
-        CommonWidgets.buildDropdownField(
-          'Confirm Installation Clearance of: Minimum of 2\' (.61m) for clearance of Motor Height from Rail AND Motor Gear Housing assembly width',
-          ['Yes', 'No'],
-        ),
-        CommonWidgets.buildDropdownField('3-Station Push Button Switch', ['Yes', 'No']),
-      ],
-    );
-  }
-
-  Widget buildNewMonitoringSystem() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CommonWidgets.buildSectionDivider(),
-        CommonWidgets.buildDropdownField('Connecting to Existing Monitoring', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Add New Monitoring System', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildSectionDivider(),
-      ],
-    );
-  }
-
-  Widget buildMonitoringFeatures() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CommonWidgets.buildSectionDivider(),
-        CommonWidgets.buildDropdownField('Drive Motor Amp', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Drive Take-up-Air', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Take-Up Distance', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Drive Motor Temp', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Drive Motor Vibration', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Bent or Missing Trolley detect', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildSectionDivider(),
+        CommonWidgets.buildTextField(
+            'Control Voltage (Volts/hz)', controlVoltage),
       ],
     );
   }
@@ -152,38 +115,30 @@ class _ConfigurationSectionState extends State<ConfigurationSection> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-       CommonWidgets. buildSectionDivider(),
-        CommonWidgets.buildDropdownField('Lubrication from the Side of Chain', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Lubrication from the Top of Chain', [
-          'Yes',
-          'No'
-        ]),
-        CommonWidgets.buildDropdownField('Is the Conveyor Chain Clean?', [
-          'Yes',
-          'No'
-        ]),
+        CommonWidgets.buildSectionDivider(),
+        CommonWidgets.buildTextField(
+            "Current Lubrication Equipment (Brand)", lubeEquipment),
+        CommonWidgets.buildTextField("Current Lubrication Type", lubeType),
+        CommonWidgets.buildTextField(
+            "Current Lubrication Viscosity/Grade", lubeGrade),
+        CommonWidgets.buildDropdownField(
+            "Side Chain Lubrication", ["Yes", "No"]),
+        CommonWidgets.buildDropdownField(
+            "Top Chain Lubrication", ["Yes", "No"]),
         CommonWidgets.buildSectionDivider(),
       ],
     );
   }
-  
+
   Widget buildController() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CommonWidgets.buildSectionDivider(),
-        CommonWidgets.buildDropdownField('Measurement Units', [
-          'Feet',
-          'Inches', 
-          'm Meter', 
-          'mm Milimeter'
-        ]),
-        CommonWidgets.buildTextField('Enter 4 Conductor Number Here',conductor4),
-        CommonWidgets.buildTextField('Enter 7 Conductor Number Here',conductor7),
-        CommonWidgets.buildTextField('Enter 2 Conductor Number Here',conductor2),
+        CommonWidgets.buildTextField(
+            "Special Options to Add on to Controller, I/O Link, Plug and Play, Dry Contacts (please specify)",
+            specialOptions),
+        CommonWidgets.buildTextField("Please Specify", specs),
         CommonWidgets.buildSectionDivider(),
       ],
     );
@@ -194,18 +149,19 @@ class _ConfigurationSectionState extends State<ConfigurationSection> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CommonWidgets.buildSectionDivider(),
-        CommonWidgets.buildDropdownField('Measurement Units', [
-          'Feet',
-          'Inches', 
-          'm Meter', 
-          'mm Milimeter'
-        ]),
-        CommonWidgets.buildTextField('Enter 4 Conductor Number Here',conductor4),
-        CommonWidgets.buildTextField('Enter 7 Conductor Number Here',conductor7),
-        CommonWidgets.buildTextField('Enter 2 Conductor Number Here',conductor2),
+        CommonWidgets.buildDropdownField(
+            'Measurement Units', ['Feet', 'Inches', 'm Meter', 'mm Milimeter']),
+        CommonWidgets.buildTextField(
+            'Enter 2 Conductor Number Here', conductor2),
+        CommonWidgets.buildTextField(
+            'Enter 4 Conductor Number Here', conductor4),
+        CommonWidgets.buildTextField(
+            'Enter 7 Conductor Number Here', conductor7),
+        CommonWidgets.buildTextField(
+            'Enter 12 Conductor Number Here', conductor12),
+        CommonWidgets.buildTextField('Junction Box Quantities', junctBoxQuant),
         CommonWidgets.buildSectionDivider(),
       ],
     );
   }
 }
-
