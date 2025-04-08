9ff7227aea952985767e79f32ca55d11e1e6be52
Baymax <addisonbg16@gmail.com>
2025-04-08 08:44:26 -0500

    Adding Measurement widget to 2100L

--- lib/industrial/10. OHPRLB (20)/CLS (7)/2100L/UI/configuration_page.dart
+++ lib/industrial/10. OHPRLB (20)/CLS (7)/2100L/UI/configuration_page.dart
@@ -27,6 +27,9 @@ class _ConfigurationSectionState extends State<ConfigurationSection> {
   final TextEditingController conductor2 = TextEditingController();
   final TextEditingController conductor12 = TextEditingController();
   final TextEditingController junctBoxQuant = TextEditingController();
+  final TextEditingController hHeight = TextEditingController();
+  final TextEditingController bDiameter = TextEditingController();
+  final TextEditingController gWidth = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +52,8 @@ class _ConfigurationSectionState extends State<ConfigurationSection> {
               CommonWidgets.buildGradientButton(context,
                   'Conveyor Specifications', buildConveyorSpecifications()),
               CommonWidgets.buildGradientButton(context, 'Wire', buildWire()),
+              CommonWidgets.buildGradientButton(
+                  context, 'Overhead Power Rail: Measurements', buildMeasurements())
             ],
           ),
         ),
@@ -196,4 +201,53 @@ class _ConfigurationSectionState extends State<ConfigurationSection> {
       ],
     );
   }
+
+  Widget buildMeasurements() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CommonWidgets.buildSectionDivider(),
+        CommonWidgets.buildDropdownField('Measurement Units', [
+          'Feet',
+          'Inches', 
+          'm Meter', 
+          'mm Milimeter'
+        ]),
+
+        // Image B
+        CommonWidgets.buildMeasurementFieldWithImage(
+        context: context,
+        title: "Overhead Power MonoRail Power Trolley Wheel (B)",
+        hintText: "Diameter",
+        imagePath: '', // there is no image on the site for this one
+        controller: bDiameter,
+        subHint: "(Diameter)",
+      ),
+
+        // Image G
+        CommonWidgets.buildMeasurementFieldWithImage(
+        context: context,
+        title: "Overhead Power MonoRail Power Rail (G)",
+        hintText: "Width",
+        imagePath: '', // there is no image on the site for this one
+        controller: gWidth,
+        subHint: "(Width)",
+      ),
+
+        // Image H
+        CommonWidgets.buildMeasurementFieldWithImage(
+        context: context,
+        title: "Overhead Power MonoRail Power Rail (H)",
+        hintText: "Height",
+        imagePath: '', // there is no image on the site for this one
+        controller: hHeight,
+        subHint: "(Height)",
+      ),
+
+        CommonWidgets.buildSectionDivider(),
+      ],
+    );
+  }
+
+
 }
